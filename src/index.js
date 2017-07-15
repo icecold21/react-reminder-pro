c5848637db1cfca0921bb5877345bb2375d045c0
Jonathan <jonathan.wylliem@jurnal.id>
2017-07-15 16:00:13 +0700

    Lecture 44 - Providing a Store and Introducing Reducers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MainApp from './components/MainApp';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <MainApp />, document.getElementById('root')
+  // Provider is providing data / state store to your application.
+  // Within Redux all application state exist as a store in single objec, reducer do is take the state and modify the state in pure way.
+  // You never mutate the state directly
+  <Provider store={ store }>
+    <MainApp />
+  </Provider>, document.getElementById('root')
 );
+
+
